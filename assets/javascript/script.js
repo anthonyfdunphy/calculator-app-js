98aeade20621e488934d2c10208b0ed18b22ec5e
anthonyfdunphy <anthonyfdunphy@gmail.com>
2022-06-15 16:07:31 +0000

    js added

---
+++ assets/javascript/script.js
@@ -0,0 +1,35 @@
+const calculator = document.querySelector('calculator')
+const keys = calculator.querySelector('.calculatorKeyboard')
+const display = calculator.querySelector('.calculatorDisplay')
+
+keys.addEventListener('click', event => {
+    if(!event.target.closest('button')) return 
+
+    const key = event.target
+    const keyValue = key.textContent
+
+    const displayValue = display.textContent
+
+    /* code for number keys*/
+    if(key.classList.contains('num')){
+        if(displayValue == '0'){
+            display.textContent = keyValue
+        } else{
+            display.textContent = displayValue + keyValue 
+        }
+    }
+
+    /* code for operator keys*/
+    if(key.classList.contains('op')){
+        console.log(key)
+        
+    }
+
+
+    
+
+   
+
+    
+    
+})
